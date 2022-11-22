d811215cd37f0906e0c177c024fbf30705e7de06
jamy165 <115711917+clarinha1197@users.noreply.github.com>
2022-11-22 09:47:50 -0300

    eu sou PAN

---
+++ Juridico/Juridico/Models/Cidade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juridico.Models
+{
+    class Cidade
+    {
+        private int codigo { get; set; }
+        private string descricao { get; set; }
+        private Estado estado { get; set; }
+
+        public List<Cidade> getTodasCidades()
+        {
+            return null;
+        }
+        public Cidade getRetomarCidade(int idCidade)
+        {
+            return null;
+        }
+        public List<Cidade> getCidadesPorEstado(Estado estado)
+        {
+            return null;
+        }
+    }
+}
---
+++ Juridico/Juridico/Models/Estado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juridico.Models
+{
+    class Estado
+    {
+        private int codigo { get; set; }
+        private string descricao { get; set; }
+        private string sigla { get; set; }
+
+        public List<Estado> getTodosEstado()
+        {
+            return null;
+        }
+        public Estado getRetomarEstado(int idEstado)
+        {
+            return null;
+        }
+    }
+}
